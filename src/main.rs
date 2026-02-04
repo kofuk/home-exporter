9bcd5a820cd330e670ae0c1d4c03089adff3da66
Koki Fukuda <ko.fu.dev@gmail.com>
2026-02-04 22:28:35 +0900

    hoge

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,10 @@
 #![no_main]
 
 use core::ptr::addr_of_mut;
-use cyw43::Control;
-use cyw43::bluetooth::BtDriver;
 use cyw43_pio::{PioSpi, RM2_CLOCK_DIVIDER};
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_net_wiznet::Device;
+use embassy_rp::Peripherals;
 use embassy_rp::bind_interrupts;
 use embassy_rp::gpio::{Level, Output};
 use embassy_rp::peripherals::{DMA_CH0, PIO0};
@@ -42,12 +40,7 @@ async fn cyw43_task(
     runner.run().await
 }
 
-#[cfg(any(feature = "bluetooth", feature = "wifi"))]
-async fn init_net_device(
-    spawner: Spawner,
-) -> (Device<'static>, Option<BtDriver<'static>>, Control<'static>) {
-    let p = embassy_rp::init(Default::default());
-
+async fn init_net_device(p: Peripherals, spawner: Spawner) {
     let fw = include_bytes!("../firmware/43439A0.bin");
     let clm = include_bytes!("../firmware/43439A0_clm.bin");
     #[cfg(feature = "bluetooth")]
@@ -70,31 +63,24 @@ async fn init_net_device(
     static STATE: StaticCell<cyw43::State> = StaticCell::new();
     let state = STATE.init(cyw43::State::new());
 
-    #[cfg(feature = "bluetooth")]
-    let (net_device, bt_device, mut control, runner) = {
+    let (_net_device, bt_device, mut control, runner) = {
         let (net_device, bt_device, control, runner) =
             cyw43::new_with_bluetooth(state, pwr, spi, fw, btfw).await;
-        (net_device, Some(bt_device), control, runner)
-    };
-    #[cfg(feature = "wifi")]
-    let (net_device, bt_device, mut control, runner) = {
-        let (net_device, control, runner) = cyw43::new(state, pwr, spi, fw).await;
-        (net_device, None, control, runner)
+        (net_device, bt_device, control, runner)
     };
 
     unwrap!(spawner.spawn(cyw43_task(runner)));
     control.init(clm).await;
 
-    return (net_device, bt_device, control);
+    let controller: ExternalController<_, 10> = ExternalController::new(bt_device);
+    run(controller).await;
 }
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     init_global_allocator();
 
-    #[cfg(any(feature = "bluetooth", feature = "wifi"))]
-    let (_net_device, bt_device, _control) = init_net_device(spawner).await;
+    let p = embassy_rp::init(Default::default());
 
-    let controller: ExternalController<_, 10> = ExternalController::new(bt_device.unwrap());
-    run(controller).await;
+    init_net_device(p, spawner).await;
 }
